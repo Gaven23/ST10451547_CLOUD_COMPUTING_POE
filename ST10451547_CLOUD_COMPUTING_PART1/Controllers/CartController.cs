aab3d39d671d89b27d879bf93e7568a1f70b26fd
Omphile Mojahi <Omphile.Mojahi@specsavers.co.za>
2024-04-04 18:37:14 +0200

    Add project files.

---
+++ ST10451547_CLOUD_COMPUTING_PART1/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10451547_CLOUD_COMPUTING_PART1.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CartController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
